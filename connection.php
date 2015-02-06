5a533451c243ffdec4754f0b8bfdc558b9cd5716
Asapnash <wiliam.williams@live.com>
2015-02-06 13:52:55 +0000

    connection

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$con = mysql_connet("194.81.104.22", "team5", "group5","db_team5") or die("couldn't connect to server!");
+
+if (mysqli_connect_errno())
+   {
+   echo "couldn't connect to server! " . mysqli_connect_error();
+   } 
+   
+?>
