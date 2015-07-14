21495448dd42cd43bb193df2803ec2ae1666fa2c
Asapnash <wiliam.williams@live.com>
2015-07-14 06:23:16 +0100

    hi

--- Contact.php
+++ Contact.php
@@ -1,57 +1,55 @@
-
 <?php
-
-// Set email variables
-$email_to = 'youremail@address.com';
+/* Set email variables*/
+$email_to = 'maze.faze.5@outlook.com';
 $email_subject = 'Form submission';
 
-// Set required fields
+/* Set required fields*/
 $required_fields = array('fullname','email','comment');
 
-// set error messages
+/* set error messages*/
 $error_messages = array(
 	'fullname' => 'Please enter a Name to proceed.',
 	'email' => 'Please enter a valid Email Address to continue.',
 	'comment' => 'Please enter your Message to continue.'
 );
 
-// Set form status
+/* Set form status*/
 $form_complete = FALSE;
 
-// configure validation array
+/* configure validation array*/
 $validation = array();
 
-// check form submittal
+/* check form submittal*/
 if(!empty($_POST)) {
-	// Sanitise POST array
+	/* Sanitise POST array*/
 	foreach($_POST as $key => $value) $_POST[$key] = remove_email_injection(trim($value));
 	
-	// Loop into required fields and make sure they match our needs
+	/* Loop into required fields and make sure they match our needs*/
 	foreach($required_fields as $field) {		
-		// the field has been submitted?
+		/* the field has been submitted?*/
 		if(!array_key_exists($field, $_POST)) array_push($validation, $field);
 		
-		// check there is information in the field?
+		/* check there is information in the field?*/
 		if($_POST[$field] == '') array_push($validation, $field);
 		
-		// validate the email address supplied
+		/* validate the email address supplied*/
 		if($field == 'email') if(!validate_email_address($_POST[$field])) array_push($validation, $field);
 	}
 	
-	// basic validation result
+	/* basic validation result*/
 	if(count($validation) == 0) {
-		// Prepare our content string
+		/* Prepare our content string
 		$email_content = 'New Website Comment: ' . "\n\n";
 		
-		// simple email content
+		/*simple email content*/
 		foreach($_POST as $key => $value) {
 			if($key != 'submit') $email_content .= $key . ': ' . $value . "\n";
 		}
 		
-		// if validation passed ok then send the email
+		/* if validation passed ok then send the email*/
 		mail($email_to, $email_subject, $email_content);
 		
-		// Update form switch
+		/* Update form switch*/
 		$form_complete = TRUE;
 	}
 }
@@ -65,24 +63,22 @@ function remove_email_injection($field = FALSE) {
 }
 
 ?>
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
 	<title>Contact Form</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link href="main.css" rel="stylesheet" type="text/css">
-<link href="SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css">
+	
+	<link href="contact/css/contactform.css" rel="stylesheet" type="text/css" />
 	
 	<script type="text/javascript">
 		var nameError = '<?php echo $error_messages['fullname']; ?>';
 		var emailError = '<?php echo $error_messages['email']; ?>';
 		var commentError = '<?php echo $error_messages['comment']; ?>';
 	</script>
-    
-<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
-
-</head>
+    </head>
 <div align="center">
 <body>
 
